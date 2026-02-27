92ea066764be3d7b2c1c9153dcdd8cc229f61678
RuchithaVarma <ruchivarma1206@gmail.com>
2026-02-27 04:13:32 -0500

    vedantu files ui

--- app/shop/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShopClient from '@/components/ShopClient';
-import { getProducts } from '@/lib/firestore/products';
-
-export const dynamic = 'force-dynamic';
-
-export default async function ShopPage() {
-  const products = await getProducts();
-  return <ShopClient initialProducts={products} />;
-}
